fda1e4e979b921401409c60091a505601be3e1d1
Matt Kiernander <mknander6@gmail.com>
2018-08-15 20:20:46 +0200

    Added ability request to modify an existing point.

--- routes/api/points.js
+++ routes/api/points.js
@@ -4,16 +4,16 @@ const router = express.Router();
 // Point Model
 const Point = require('../../models/Point');
 
-// @route   GET api/items
-// @desc    Get All Items
+// @route   GET api/points
+// @desc    Get All Points
 // @access  Public
 router.get('/', (req, res) => {
   Point.find()
     .then(point => res.json(Point));
 });
 
-// @route   POST api/items
-// @desc    Create An Item
+// @route   POST api/points
+// @desc    Create An Point
 // @access  Public
 router.post('/', (req, res) => {
   const newPoint = new Point({
@@ -23,13 +23,10 @@ router.post('/', (req, res) => {
   newPoint.save().then(point => res.json(point));
 });
 
-// @route   DELETE api/items/:id
-// @desc    Delete A Item
-// @access  Public
-router.delete('/:id', (req, res) => {
-  Item.findById(req.params.id)
-    .then(item => item.remove().then(() => res.json({ success: true })))
-    .catch(err => res.status(404).json({ success: false }));
+router.put('/:id', (req, res) => {
+  Point.findByIdAndUpdate(req.params.id, { points: req.body.points })
+    .then(() => res.json({ success: true })
+    .catch(err => res.status(404).json({ success: false })));
 });
 
 module.exports = router;
